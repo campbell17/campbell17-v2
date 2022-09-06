c7282260c8d289ddea011b63968a6b37c5db698f
Tim Campbell <timcampbell@hey.com>
2022-09-06 19:22:40 -0400

    update font config

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,10 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,900;1,400;1,900&display=swap" 
+          <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,900;1,400;1,900&display=optional" 
             rel="stylesheet" 
           />
-          <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,700;1,400;1,700&display=swap" 
+          <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,700;1,400;1,700&display=optional" 
             rel="stylesheet" 
           />
         </Head>
