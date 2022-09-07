94d01b9e1e8ea195f4fdada58fda947faae9f7c5
Tim Campbell <timcampbell@hey.com>
2022-09-07 08:54:02 -0400

    add new analytics property code

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
@@ -11,6 +12,19 @@ class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,700;1,400;1,700&display=optional" 
             rel="stylesheet" 
           />
+          <Script 
+            src="https://www.googletagmanager.com/gtag/js?id=G-GL6B2YMJHZ" 
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-GL6B2YMJHZ');            
+            `}
+          </Script>          
         </Head>
         <body>
           <Main />
